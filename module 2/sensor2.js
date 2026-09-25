695afff9f48d4b9c8f8bc2507282b44da20b1bad
jasveena15 <kaurjasveena15@gmail.com>
2026-09-26 02:21:08 +1000

    Add files via upload

---
+++ module 2/sensor2.js
@@ -0,0 +1,28 @@
+const mqtt = require("mqtt");
+
+const client = mqtt.connect("mqtt://localhost");
+const sensorId = "sensor2";
+
+client.on("connect", () => {
+    console.log(`${sensorId} connected to MQTT broker`);
+
+    setInterval(() => {
+        const temperature = Math.floor(Math.random() * 25) + 20;
+        const smoke = Math.floor(Math.random() * 100);
+        const fire = temperature > 40 && smoke > 70;
+
+        const data = {
+            sensor: sensorId,
+            temperature: temperature,
+            smoke: smoke,
+            fire: fire
+        };
+
+        console.log(data);
+
+        client.publish(
+            "forest/sensors",
+            JSON.stringify(data)
+        );
+    }, 3000);
+});
